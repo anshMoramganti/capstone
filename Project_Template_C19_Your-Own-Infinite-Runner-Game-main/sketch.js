59ec8026990439116e5547387f1c703a34c7736e
anshMoramganti <116480620+anshMoramganti@users.noreply.github.com>
2022-12-24 11:30:32 +1030

    Add files via upload

---
+++ Project_Template_C19_Your-Own-Infinite-Runner-Game-main/sketch.js
@@ -0,0 +1,24 @@
+var box
+
+function preload(){
+
+
+
+
+}
+
+function setup() {
+createCanvas(screen.width,screen.height)
+   
+box = createSprite(300,200,20,20)
+
+}
+
+function draw() {
+ 
+
+
+
+
+    drawSprites()
+}
